a4eb3d256842bc3db1c4e1ccc2c26c8b63f8ffc9
RumiYo <yokoyamarumi@gmail.com>
2023-12-13 01:31:16 -0500

    Render data

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+  const [stockList, setStockList] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then( r => r.json() )
+    .then( data => setStockList(data) )
+  },[])
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stockList={stockList} />
         </div>
         <div className="col-4">
           <PortfolioContainer />
